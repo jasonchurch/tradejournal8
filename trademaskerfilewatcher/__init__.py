237e1531de487437004497453d8c86f1615783bf
Jason Church <jasonchurch@edeveloper.ca>
2026-01-03 19:55:09 -0500

    chore: clear trademaskerfilewatcher contents for rebuild

--- trademaskerfilewatcher/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Trade Masker package.
-
-Exposes main classes for convenience.
-"""
-from .watcher import TradeMaskerFileWatcher
-from .processors.dummy import DummyMasker
-
-__all__ = ["TradeMaskerFileWatcher", "DummyMasker"]
